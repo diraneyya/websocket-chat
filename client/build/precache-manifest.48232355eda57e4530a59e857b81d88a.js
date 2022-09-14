ed569c29d923964ce743589b3710b44d4db2f79e
Orwa Diraneyya <orwa@Orwas-MacBook-Pro.local>
2022-09-14 19:39:43 +0200

    Added built bundles of frontend

---
+++ client/build/precache-manifest.48232355eda57e4530a59e857b81d88a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0472e1ab0c4b3dcbcb7f58d131d7a4aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f7a0eccf5f245bd365f",
+    "url": "/static/css/2.a0a97cb9.chunk.css"
+  },
+  {
+    "revision": "a4051225972524a448c6",
+    "url": "/static/css/main.818586c7.chunk.css"
+  },
+  {
+    "revision": "8f7a0eccf5f245bd365f",
+    "url": "/static/js/2.3247ca40.chunk.js"
+  },
+  {
+    "revision": "cbc41762b08bce4213514217ce9f16f9",
+    "url": "/static/js/2.3247ca40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4051225972524a448c6",
+    "url": "/static/js/main.f52c3b35.chunk.js"
+  },
+  {
+    "revision": "7177cd75ecb49e65fd77",
+    "url": "/static/js/runtime-main.d4e3f5c6.js"
+  }
+]);
--- client/qawolf.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  config: "node_modules/qawolf/js-jest.config.json",
-  rootDir: "",
-  testTimeout: 60000,
-  useTypeScript: false
-}
